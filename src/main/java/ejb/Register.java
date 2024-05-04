5e9ad903ad08ec7f65431cbe15c8e858a606063d
malvikachoksi <malvikachoksi55@gmail.com>
2024-05-04 13:04:36 +0530

    ejb new file cretae register

---
+++ src/main/java/ejb/Register.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author DELL
+ */
+@Stateless
+public class Register implements RegisterLocal {
+
+    public void demo(){
+        
+    }
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
